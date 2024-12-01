34819d79110256f2601a6396c909e4c8582f7913
Jahongir15-dev <jahongirtorayev1507@gmail.com>
2024-12-01 21:36:13 +0500

    Firs push

---
+++ src/main/java/app/portfolio/spring_boot_oauth2_login/SpringBootOauth2LoginApplication.java
@@ -0,0 +1,13 @@
+package app.portfolio.spring_boot_oauth2_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2LoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/portfolio/spring_boot_oauth2_login/SpringBootOauth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package app.portfolio.spring_boot_oauth2_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
